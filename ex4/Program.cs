97c0f1128058b7d87b1b32888df7b5b1ef0306c2
murabye <vlada_kuz2011@mail.ru>
2017-06-05 01:02:03 +0500

    черновой

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MyLib;
 
 namespace ex4
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static double epsilon;
+
+        private static void Main()
         {
+            while (true)
+            {
+                epsilon = Ask.RealNum("Введите точность: ");
+                Console.WriteLine("Ответ: " + Calculate(0.5, 0.6));
+                OC.Stay();
+            }
+
+        }
+
+        static double Calculate(double x1, double x2)
+        {
+            var x3 = x1 - (x2 - x1) * Func(x1) / (Func(x2) - Func(x1));
+
+            return Math.Abs(x3 - x2) > epsilon ? Calculate(x2, x3) : x3;
+        }
+
+        static double Func(double arg)
+        {
+            return arg * arg - Math.Sin(5 * arg);
         }
     }
 }
